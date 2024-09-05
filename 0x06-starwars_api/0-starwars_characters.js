46a3a706f7b330447c248d04af6d2b212690ef25
Luciano7 <lucky.h.fatch@gmail.com>
2024-09-05 10:52:57 +0000

    starwars js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,40 +2,16 @@
 
 const request = require('request');
 
-const movieId = process.argv[2];
-if (!movieId) {
-  console.error('Please provide a movie ID as the first argument.');
-  process.exit(1);
-}
-
-request('https://swapi-api.hbtn.io/api/films/' + movieId, function (err, res, body) {
-  if (err) {
-    console.error('Error fetching movie:', err);
-    return;
-  }
-
-  const data = JSON.parse(body);
-  
-  if (res.statusCode !== 200 || !data.characters) {
-    console.error(`Movie with ID ${movieId} not found.`);
-    return;
-  }
-
-  const actors = data.characters;
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
+  if (err) throw err;
+  const actors = JSON.parse(body).characters;
   exactOrder(actors, 0);
 });
-
 const exactOrder = (actors, x) => {
   if (x === actors.length) return;
-
   request(actors[x], function (err, res, body) {
-    if (err) {
-      console.error('Error fetching character:', err);
-      return;
-    }
-
+    if (err) throw err;
     console.log(JSON.parse(body).name);
-
     exactOrder(actors, x + 1);
   });
 };
